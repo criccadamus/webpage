6167b49e1968e083e1f470affca22e46f63bb5f6
Jorge Fernando Álava Vélez <services@criccadam.us>
2023-01-01 04:21:16 +0100

    More tweaks

---
+++ resources/scripts/return-streams.js
@@ -0,0 +1,10 @@
+const div = document.querySelector('.streams-title');
+
+window.addEventListener('scroll', function() {
+  if (window.pageYOffset > div.offsetTop) {
+    div.classList.add('is-fixed');
+  } else {
+    div.classList.remove('is-fixed');
+    div.classList.add('is-returning');
+  }
+});
